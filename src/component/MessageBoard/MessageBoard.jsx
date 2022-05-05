c4ac6945c913dfa33a06cea599aba004227edb6d
orlandouchiha425 <valadez425@gmail.com>
2022-05-04 20:24:34 -0400

    got my useEffect 9%done

--- src/component/MessageBoard/MessageBoard.jsx
+++ src/component/MessageBoard/MessageBoard.jsx
@@ -1,11 +1,45 @@
 import { useState,useEffect } from "react"
-import styles from "./MessageBoard.module.css"
+
+import { io } from "socket.io-client"
+const socket = io("https://localhost:3005");
+
+
+// import styles from "./MessageBoard.module.css"
 
 
 
 
 export default function MessageBoard(){
+  const [chatRoom, setChatroom] = useState({
+    ChatRoom: " "
+      })
 
+  const [messages, setMessages] = useState([])
+  const [text,setText]=useState("")
+
+
+  useEffect(() => {
+    socket.io()
+
+    socket.on("join",function(room){
+    setChatroom({...chatRoom,["ChatRoom"]:room})
+
+    socket.on("receive",function(message){
+      console.log(message);
+    if(messages.length<9){
+      setMessages(...message,message)
+    }else{
+      setMessages(delete(message));
+      setMessages(...messages,message)
+    }
+    for (i=0;i<messages.length;i++){
+      setText()
+    }
+
+    })
+
+    })
+  },[])
 
 
 
@@ -25,7 +59,7 @@ export default function MessageBoard(){
         <input placeholder="select a room..." className = "form-control "  type = "text"/>
         
         <input className="btn btn-primary" type = "submit" value = "Connect" onclick = "Connect()"/>
-       
+        {messages.length && messageList}
  
        
       </div>
--- src/utilities/script.js
+++ src/utilities/script.js
@@ -9,7 +9,9 @@ let chatRoom;
 let messages = [];
 
 function onload(){
+    socket.io()
     const [chatRoom,setChatroom]=useState("")
+    
 socket.on("join",function(room){
     setChatroom(...chatRoom,chatRoom)= `ChatRoom: ${room}`
 
@@ -26,10 +28,42 @@ socket.on("receive",function(message){
 const [messages,setMessages]=useState([])
     console.log(message);
     if (messages.length<9){
-        setMessages(...message);
+        setMessages(...messages);
 
     }
     else{
-        
+
     }
 })
+
+
+
+// function onload(){
+//     socket = io();
+//     usernameInput = document.getElementById("NameInput");
+//     chatIDInput = document.getElementById("IDInput");
+//     messageInput = document.getElementById("ComposedMessage");
+//     chatRoom = document.getElementById("RoomID");
+//     dingSound = document.getElementById("Ding");
+  
+//     socket.on("join", function(room){
+//       chatRoom.innerHTML = "Chatroom : " + room;
+//     })
+  
+//     socket.on("recieve", function(message){
+//       console.log(message);
+//       if (messages.length < 9){
+//         messages.push(message);
+//         dingSound.currentTime = 0;
+//         dingSound.play();
+//       }
+//       else{
+//         messages.shift();
+//         messages.push(message);
+//       }
+//       for (i = 0; i < messages.length; i++){
+//           document.getElementById("Message"+i).innerHTML = messages[i];
+//           document.getElementById("Message"+i).style.color = "#303030";
+//       }
+//     })
+//   }
